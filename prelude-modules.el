dff021838954a77fea58bd67a165a7c78b296f7b
Jesse Oldroyd <oldroyd.j@wvwc.edu>
2025-11-05 14:30:56 -0500

    Add EPUB mode.

--- prelude-modules.el
+++ prelude-modules.el
@@ -64,11 +64,11 @@
 ;; Modules for a few very common programming languages
 ;; are enabled by default.
 
-(require 'prelude-c)
+;; (require 'prelude-c)
 ;; (require 'prelude-clojure)
 ;; (require 'prelude-coffee)
 ;; (require 'prelude-common-lisp)
-(require 'prelude-css)
+;; (require 'prelude-css)
 ;; (require 'prelude-dart)
 (require 'prelude-emacs-lisp)
 ;; (require 'prelude-erlang)
@@ -76,14 +76,14 @@
 ;; (require 'prelude-fsharp)
 ;; (require 'prelude-go)
 ;; (require 'prelude-haskell)
-(require 'prelude-js)
+;; (require 'prelude-js)
 (require 'prelude-latex)
 (require 'prelude-lisp) ;; Common setup for Lisp-like languages
 ;; (require 'prelude-literate-programming) ;; Setup for Literate Programming
 (require 'prelude-lsp) ;; Base setup for the Language Server Protocol
 ;; (require 'prelude-lua)
 ;; (require 'prelude-ocaml)
-(require 'prelude-perl)
+;; (require 'prelude-perl)
 ;; (require 'prelude-python)
 ;; (require 'prelude-racket)
 ;; (require 'prelude-ruby)
@@ -93,12 +93,12 @@
 (require 'prelude-shell)
 ;; (require 'prelude-scss)
 ;; (require 'prelude-ts)
-(require 'prelude-web) ;; Emacs mode for web templates
+;; (require 'prelude-web) ;; Emacs mode for web templates
 (require 'prelude-xml)
 (require 'prelude-yaml)
 
 ;;; Misc
-(require 'prelude-erc) ;; A popular Emacs IRC client (useful if you're still into Freenode)
+;; (require 'prelude-erc) ;; A popular Emacs IRC client (useful if you're still into Freenode)
 
 (provide 'prelude-modules)
 ;;; prelude-modules.el ends here
